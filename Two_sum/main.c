79188e79e5c2437f3daea0e87b578d3851732120
Tu-Wei_Chen <asd415305@gmail.com>
2022-10-21 16:46:16 +0800

    * Update Two Sum another method with sorting + Add a struct to save origin idx + Add Binary Search method

--- Two_sum/main.c
+++ Two_sum/main.c
@@ -14,6 +14,14 @@ typedef struct
     int val;
     int idx;
 } NUMIDX_T;
+
+
+
+NUMIDX_T numidx[2] =
+{
+    {.val = 10, .idx = 1},
+    {.val = 7 , .idx = 2},
+};
 /**
  Target :
  Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
@@ -45,6 +53,16 @@ int compareInt(const void *var1, const void *var2)
     return ret;
 }
 
+int compareStruct(const void *var1, const void *var2)
+{
+    int comResult = 0;
+    int val1 = ((NUMIDX_T *)var1)->val;
+    int val2 = ((NUMIDX_T *)var2)->val;
+
+    if (val1 > val2) comResult = 1;
+
+    return comResult;
+}
 
 /*
 // This Algorithm has 2 action 1. compare 2. swap
@@ -123,6 +141,36 @@ void BubbleSort(void *base, size_t n, size_t size, int (*compare)(const void *, 
     }
 }
 
+
+static int BinarySearch(const NUMIDX_T *ordernums, int leftidx, int rightidx, int target)
+{
+    int ret = -1;
+    if ((ordernums[leftidx].val > target) || (ordernums[rightidx].val < target)) return -1;
+
+    while(leftidx <= rightidx)
+    {
+        int mididx = (leftidx + rightidx) / 2;
+        if ( target < ordernums[mididx].val)
+        {
+            rightidx = (mididx - 1);
+        }
+        else if (ordernums[mididx].val < target)
+        {
+            leftidx = (mididx + 1);
+        }
+        else
+        {
+            ret = mididx;
+            return ret;
+        }
+
+    }
+
+    return ret;
+}
+
+// Common Force Search method
+/*
 int* twoSum(int* nums, int numsSize, int target, int* returnSize)
 {
     unsigned int index1, index2;
@@ -149,23 +197,65 @@ int* twoSum(int* nums, int numsSize, int target, int* returnSize)
 
     return array;
 }
+*/
+
+int* twoSum(int* nums, int numsSize, int target, int* returnSize)
+{
+    NUMIDX_T numswithIdx[numsSize];
+    int nowIdx = 0;
+    int sum;
+    int *array = malloc(sizeof(int) * 2);
+    array[0] = -1;
+    array[1] = -1;
+
+    for (nowIdx = 0; nowIdx < numsSize; nowIdx++)
+    {
+        numswithIdx[nowIdx].idx = nowIdx;
+        numswithIdx[nowIdx].val = nums[nowIdx];
+    }
+    // Sorting
+    BubbleSort(numswithIdx, numsSize, sizeof(NUMIDX_T), compareStruct);
+    int index = 0;
+    for (index = 0; index < numsSize; index++)
+    {
+        printf("val:%2d,idx:%2d\n", numswithIdx[index].val, numswithIdx[index].idx);
+    }
+
+    for (nowIdx = 0; nowIdx < numsSize-1; nowIdx++)
+    {
+        int searchVal = target - numswithIdx[nowIdx].val;
+        int idx2 =  BinarySearch(numswithIdx, (nowIdx + 1), (numsSize - 1), searchVal);
+
+        if (idx2 >= 1)
+        {
+            array[0] = numswithIdx[nowIdx].idx;
+            array[1] = numswithIdx[idx2].idx;
+            break;
+        }
+    }
+    return array;
+
+
+
+}
 int main(void)
 {
 
     int numsSize = 9;
     int nums[] = {15, -2, 12, 3, -1, 10, -9, 14, 21};
-    int target = 26;
+    int target = 9;
     int returnSize = 0;
 
     int *resultidx = twoSum(nums, numsSize, target, &returnSize);
 
-    printf("target:%3d == (%3d + %3d)\n", target , nums[resultidx[0]], nums[resultidx[1]]);
+    printf("target:%3d idx:(%2d  %2d)\n", target , resultidx[0], resultidx[1]);
 
     free(resultidx);
 
 
 
-    /* Test Sort */
+    /*
+    // Test common bubble Sort
     BubbleSort(nums, numsSize, sizeof(int), compareInt);
 
     int index = 0;
@@ -173,8 +263,40 @@ int main(void)
     {
         printf("%2d\n", nums[index]);
     }
+    */
+
+    /*
+    // Test Special Struct Bubble Sort
+    int index = 0;
+
+    NUMIDX_T numsWithIdx[numsSize];
+
+    for (index = 0; index < numsSize; index++)
+    {
+        numsWithIdx[index].val = nums[index];
+        numsWithIdx[index].idx = index;
+    }
 
 
+    for (index = 0; index < numsSize; index++)
+    {
+        printf("val:%2d,idx:%2d\n", numsWithIdx[index].val, numsWithIdx[index].idx);
+    }
+
+
+    BubbleSort(numsWithIdx, numsSize, sizeof(NUMIDX_T), compareStruct);
+    //printf("%2d", (int )sizeof(NUMIDX_T));
+    printf("----------\n");
+    for (index = 0; index < numsSize; index++)
+    {
+        printf("val:%2d,oldidx:%2d, newidx:%2d\n", numsWithIdx[index].val, numsWithIdx[index].idx, index);
+    }
+
+
+    // Test Binary Seach in specail struct
+    int ack = BinarySearch(numsWithIdx, 0, 8, 21);
+    printf("%2d\n", ack);
+    */
     /* Test Compare function
 
     int comVal1 = 12, comVal2 = 11;
