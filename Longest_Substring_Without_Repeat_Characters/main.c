fd0db4849c72873d7e5f2f82acee009b383e9f57
Hsin-Hsuan Yu <hsinhsuanyu@gmail.com>
2022-10-26 00:17:13 +0800

    + Add new taget Longest Substring Without Repeating Characters

---
+++ Longest_Substring_Without_Repeat_Characters/main.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+/*
+Taget : Given a string s, find the length of the longest substring without repeating characters.
+
+example 1
+Input: s = "abcabcbb"
+Output: 3
+Explanation: The answer is "abc", with the length of 3.
+
+example 2
+Input: s = "bbbbb"
+Output: 1
+Explanation: The answer is "b", with the length of 1.
+
+example 3
+Input: s = "pwwkew"
+Output: 3
+Explanation: The answer is "wke", with the length of 3.
+Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+
+
+Condtion : Can use to Calculate O(f(N))
+
+
+    0 <= s.length <= 5 * 104
+    s consists of English letters, digits, symbols and spaces.
+
+*/
+
+
+int lengthOfLongestSubstring(char * s)
+{
+    return 0;
+
+}
+
+
+int main(void)
+{
+
+    printf("Hello\n");
+    return 1;
+}
