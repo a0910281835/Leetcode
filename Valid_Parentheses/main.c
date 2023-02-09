ba2f26a0dd76b2544d00ce35c9817d9657d25a09
Wei-Chen Tu <asd415305@gmail.com>
2023-02-09 22:56:33 +0800

    * Update function

--- Valid_Parentheses/main.c
+++ Valid_Parentheses/main.c
@@ -1,28 +1,40 @@
-/*
- * 20. Valid Parentheses
- *
- *
- * Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
- *
- * An input string is valid if:
- *
- * Open brackets must be closed by the same type of brackets.
- * Open brackets must be closed in the correct order.
- * Every close bracket has a corresponding open bracket of the same type.
- *
- *
- *
- *
- * Example 1:
- *
- * Input: s = "()"
- * Output: true
- * */
-
+ /** 20. Valid Parentheses
+  *
+  *
+  * Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+  *
+  * An input string is valid if:
+  *
+  * Open brackets must be closed by the same type of brackets.
+  * Open brackets must be closed in the correct order.
+  * Every close bracket has a corresponding open bracket of the same type.
+  *
+  *
+  *
+  *
+  * Example 1:
+  *
+  * Input: s = "()"
+  * Output: true
+ **/
 #include "stdio.h"
 #include"stdlib.h"
+#define MAX_SIZE 1000
+
+typedef unsigned char bool;
+
+typedef struct STACK_CHAR_t * P_STACK_CHAR_t;
+
+typedef struct STACK_CHAR_t
+{
+    char array[MAX_SIZE];
+    int current_num;
+
+}STACK_CHAR_t;
+
 bool isValid(char * s)
 {
+
 
 }
 
