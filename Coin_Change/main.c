4b7198f563f5dc35c5cd216d4a04cc9b5a2342db
Tu-Wei_Chen <asd415305@gmail.com>
2023-02-11 00:33:25 +0800

    * Use Dynamic Programing to Solve

--- Coin_Change/main.c
+++ Coin_Change/main.c
@@ -3,6 +3,9 @@
 
 #define ERROR -1
 
+// Use Recursive but efficiency is bad
+// Since High to Low
+ /*
 int coinChange(int* coins, int coinsSize, int amount)
 {
     //Inital Conditon
@@ -48,15 +51,53 @@ int coinChange(int* coins, int coinsSize, int amount)
     return recordUseSmallest;
 
 }
+*/
+// Version : Consumes Space.
+int coinChange(int* coins, int coinsSize, int amount)
+{
+    //Dymaic Progamming
+    int totalMoney = 0;
+    int recordSmallestNumForThisAmount[amount + 1];
+
+    //inital Condition
+    recordSmallestNumForThisAmount[0] = 0;
+
+    for (totalMoney = 1; totalMoney <= amount; totalMoney++)
+    {
+        int tempMin = 10001;// amount <= 10000
+        int idx = 0;
+
+        for (idx = 0; idx < coinsSize; idx++)
+        {
+            int dividMoney = (totalMoney - coins[idx]);
+
+
+            if (dividMoney < 0 || (recordSmallestNumForThisAmount[dividMoney] == -1))
+                continue;
+            else
+            {
+                tempMin = (tempMin > (1 + recordSmallestNumForThisAmount[dividMoney])) ? (1 + recordSmallestNumForThisAmount[dividMoney]) : tempMin;
+            }
+
+        }
+
+        if (tempMin == 10001) recordSmallestNumForThisAmount[totalMoney] = -1;
+        else                  recordSmallestNumForThisAmount[totalMoney] = tempMin;
+
+    }
+
+    return  recordSmallestNumForThisAmount[amount];
+}
+
 int main(void)
 {
 
     int coins[3] = {1, 2, 5};
     int size = 3;
-    int amount = 13;
+    int amount = 15;
 
     int ret = coinChange(coins, size, amount);
-    printf("%2d\n", ret);
+    printf("Use : %2d\n", ret);
 
     return 1;
 }
