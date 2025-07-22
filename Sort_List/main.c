ed0b29239f73b5dcd533554b1300cf2958d8df38
Hsin-Hsuan Yu <hsinhsuanyu@gmail.com>
2025-07-22 23:16:33 +0800

    * Update Merge Sort and Add Quick sort

--- Sort_List/main.c
+++ Sort_List/main.c
@@ -273,6 +273,153 @@ struct ListNode* reverseBetween(struct ListNode* head, int left, int right)
     head   = head->next;
     free(pDummy);
 }
+/**
+ *  * Note: The returned array must be malloced, assume caller calls free().
+ *   */
+// 0 1 2 3 4
+// 4 2 9 0 -1
+
+int * mergeTwoArray(int *nums, int leftHeadPos, int leftArraySize, int rightHeadPos, int rightArraySize, int *recordNums)
+{
+    if (leftArraySize + rightArraySize > 1)
+    {
+        int leftIdx  = 0;
+        int rightIdx = 0;
+        int newIdx   = 0;
+
+        while ((leftIdx < leftArraySize) && (rightIdx < rightArraySize))
+        {
+            if (nums[leftHeadPos+leftIdx] <= nums[rightHeadPos+rightIdx])
+            {
+                recordNums[leftHeadPos+newIdx++] = nums[leftHeadPos+leftIdx++];
+            }
+            else
+            {
+                recordNums[leftHeadPos+newIdx++] = nums[rightHeadPos+rightIdx++];
+            }
+        }
+
+        while (leftIdx < leftArraySize)
+        {
+            recordNums[leftHeadPos+newIdx++] = nums[leftHeadPos+leftIdx++];
+        }
+        while (rightIdx < rightArraySize)
+        {
+            recordNums[leftHeadPos+newIdx++] = nums[rightHeadPos+rightIdx++];
+        }
+
+
+        newIdx = 0;
+        while (newIdx < (leftArraySize+rightArraySize))
+        {
+            nums[leftHeadPos+newIdx] = recordNums[leftHeadPos+newIdx];
+            newIdx++;
+        }
+
+    }
+
+    return nums;
+}
+
+int * mergeSortArray(int *nums, int leftHeadPos, int rightTailPos, int *recordNums)
+{
+// 0 1 2 3 4
+// 4 1 3 5 2
+// The only you need to concern is the basic condition
+// 0 1
+// 4 1
+// 0 | 1
+// recursive the only 0 and the another is 1
+    if ((rightTailPos-leftHeadPos+1) > 1)
+    {
+        int mid = ((rightTailPos-leftHeadPos + 1) >> 1);
+        //printf("leftPos:%d, Mid:%d rightTaPos%d\n",leftHeadPos, mid, rightTailPos);
+        //printf("nums:%d,%d,%d,%d,%d\n", nums[0], nums[1], nums[2], nums[3], nums[4]);
+        mergeSortArray(nums, leftHeadPos, leftHeadPos+mid-1, recordNums);// <----------------------------------this is key point
+        //printf("sortingLeft nums:%d,%d,%d,%d,%d\n", nums[0], nums[1], nums[2], nums[3], nums[4]);
+        mergeSortArray(nums, leftHeadPos+mid, rightTailPos, recordNums);
+        //printf("sortingRight nums:%d,%d,%d,%d,%d\n", nums[0], nums[1], nums[2], nums[3], nums[4]);
+        int leftArraySize  = mid;
+        int rightArraySize = rightTailPos-leftHeadPos+1-mid;
+        mergeTwoArray(nums, leftHeadPos, leftArraySize, leftHeadPos+mid, rightArraySize, recordNums);
+        printf("MergeTwoPath nums:%d,%d,%d,%d,%d\n", nums[0], nums[1], nums[2], nums[3], nums[4]);
+    }
+    //else if (1 == (rightTailPos-leftHeadPos))
+    //{
+    //    if (nums[leftHeadPos] > nums[rightTailPos])
+    //    {
+    //        int temp = nums[leftHeadPos];
+    //        nums[leftHeadPos]  = nums[rightTailPos];
+    //        nums[rightTailPos] = temp;
+    //    }
+
+    //}
+
+
+    return nums;
+}
+
+void swapData(int *first, int *second)
+{
+    int temp = *first;
+    *first   = *second;
+    *second  = temp;
+}
+
+//int findPivotFrom3Pos(int *array, int leftHeadIdx, int rightTailIdx)
+int findPivotFrom3Pos(int *array, int size)
+{
+    if (size >= 3)
+    {
+        int midIdx = size >> 1;
+        if (array[0] > array[midIdx]) swapData(array, array+midIdx);
+        if (array[0] > array[size-1]) swapData(array, array+(size-1));
+        if (array[midIdx] > array[size-1]) swapData(array+midIdx, array+(size-1));
+        return midIdx;
+    }
+    return size;
+}
+
+//void quickSortByRecursive(int *array, int leftHeadIdx, int rightTailIdx)
+void quickSortByRecursive(int *array, int size)
+{
+    if (size >= 3)
+    {
+        // How to Sorting
+        // Choose a pivot to decide this point the last position.
+        int midIdx = findPivotFrom3Pos(array, size);
+        // Divid Two Sequence
+        //In order to control code effect we put the mid to right-1
+
+        // Recursive left
+        // Recursive right
+    }
+    else
+    {
+        // Basic Conditon
+
+    }
+}
+
+
+void QuickSort(int *array, int size)
+{
+    // InterFace
+    quickSortByRecursive(array, size);
+}
+
+#define _MERGE_SORT_ARRAY
+int* sortArray(int* nums, int numsSize, int* returnSize)
+{
+#if defined(_MERGE_SORT_ARRAY)
+    int *recordNums = malloc(sizeof(int) * numsSize);
+    nums = mergeSortArray(nums, 0, numsSize-1, recordNums);
+    free(recordNums);
+#elif defined(_QUICK_SORT_ARRAY)
+#endif
+
+    return nums;
+}
 
 
 int main(void)
@@ -296,6 +443,11 @@ int main(void)
     pHead = reverseList(pHead);
     printf("reverse list : ");
     printList(pHead);
+
+
+    printf("test----sorting Array\n");
+    int *returnSize = malloc(sizeof(int));
+    sortArray(array, num, returnSize);
 
 #if defined _SORT
     pNext = pHead;
